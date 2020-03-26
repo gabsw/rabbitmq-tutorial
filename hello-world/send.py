452d7036aa4ce96f52b67e155304d7f2675e6a73
gabsw <sw.gabrielasantos@gmail.com>
2020-03-26 12:00:13 +0000

    Part 3

--- hello-world/send.py
+++ hello-world/send.py
@@ -6,6 +6,8 @@ channel = connection.channel()
 
 channel.queue_declare(queue='hello')
 
+# default exchange
+# messages are routed to the queue with the name specified by routing_key, if it exists.
 channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
 print(" [x] Sent 'Hello World!'")
 connection.close()
